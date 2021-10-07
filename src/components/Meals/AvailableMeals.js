b4e154f190012f74061a984119dfaff39f5298ee
Isisaurus <dianavitanyi@gmail.com>
2021-10-07 15:54:26 +0200

    Menu items further styles.

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,24 +7,32 @@ const DUMMY_MEALS = [
   {
     id: "m1",
     name: "Sushi",
+    image:
+      "https://images.pexels.com/photos/7243421/pexels-photo-7243421.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     description: "Finest fish and veggies",
     price: 22.99,
   },
   {
     id: "m2",
     name: "Schnitzel",
+    image:
+      "https://images.pexels.com/photos/7627422/pexels-photo-7627422.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     description: "A german specialty!",
     price: 16.5,
   },
   {
     id: "m3",
     name: "Barbecue Burger",
+    image:
+      "https://images.pexels.com/photos/9467486/pexels-photo-9467486.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     description: "American, raw, meaty",
     price: 12.99,
   },
   {
     id: "m4",
     name: "Green Bowl",
+    image:
+      "https://images.pexels.com/photos/5514819/pexels-photo-5514819.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     description: "Healthy...and green...",
     price: 18.99,
   },
@@ -36,13 +44,12 @@ const AvailableMeals = () => {
       name={meal.name}
       price={meal.price}
       description={meal.description}
+      image={meal.image}
     />
   ));
   return (
     <section className={classes.section}>
-      <Card>
-        <ul>{mealsList}</ul>
-      </Card>
+      <Card>{mealsList}</Card>
     </section>
   );
 };
--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -2,22 +2,17 @@ import React from "react";
 import classes from "./MealItem.module.css";
 
 const Mealitem = (props) => {
-  const price = `${props.price.toFixed(2)}`;
+  const price = `€${props.price.toFixed(2)}`;
+
   return (
-    <li className={classes.item}>
-      <article>
-        <div className={classes.img} />
-        <div className={classes.content}>
-          <div>
-            <h3 className={classes.title}>{props.name}</h3>
-            <p className={classes.description}>{props.description}</p>
-          </div>
-          <div>
-            <p>{price}</p>
-          </div>
-        </div>
-      </article>
-    </li>
+    <article className={classes.item}>
+      <h3 className={classes.title}>{props.name}</h3>
+      <img className={classes.img} src={props.image} alt={props.name} />
+      <div className={classes.content}>
+        <p className={classes.description}>{props.description}</p>
+        <p className={classes.price}>{price}</p>
+      </div>
+    </article>
   );
 };
 
