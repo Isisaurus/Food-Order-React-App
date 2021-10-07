f5f4628b47ab07beb1f30392d37a443216cf7fb2
Isisaurus <dianavitanyi@gmail.com>
2021-10-07 12:53:22 +0200

    CartIcon component with svg icon of cart.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
+import { Fragment } from "react";
 import "./App.css";
 import Header from "./components/Layout/Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello there</h1>
+    <Fragment>
       <Header />
-    </div>
+    </Fragment>
   );
 }
 
