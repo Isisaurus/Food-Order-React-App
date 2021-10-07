c17f7eb8f851dfcb66b07e49ef65dcc8cc245b47
Isisaurus <dianavitanyi@gmail.com>
2021-10-07 13:14:03 +0200

    Styled HeaderCartButton component.

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,7 +1,18 @@
 import React from "react";
+import CartIcon from "./../Cart/CartIcon";
+
+import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = () => {
-  return <button></button>;
+  return (
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span className={classes.label}>Your Cart</span>
+      <span className={classes.badge}>0</span>
+    </button>
+  );
 };
 
 export default HeaderCartButton;
