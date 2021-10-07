b239b259cd36e98a25cc76bfa868ea77b092e234
Isisaurus <dianavitanyi@gmail.com>
2021-10-07 14:36:22 +0200

    Meals section added.

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,21 @@
+import React from "react";
+import classes from "./MealsSummary.module.css";
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food, delivered to your doorstep.</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae laborum
+        cupiditate at architecto ducimus, accusamus temporibus odit quisquam
+        facilis? Fugit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus,
+        beatae?
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
