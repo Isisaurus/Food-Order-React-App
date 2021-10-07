7102cb0d1c47eafa07e8ebd67a2742284d6c731e
Isisaurus <dianavitanyi@gmail.com>
2021-10-07 15:59:36 +0200

    Summary title styles adjusted.

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -4,7 +4,10 @@ import classes from "./MealsSummary.module.css";
 const MealsSummary = () => {
   return (
     <section className={classes.summary}>
-      <h2>Delicious Food, delivered to your doorstep.</h2>
+      <div className={classes.title}>
+        <h2>Delicious Food, delivered to your doorstep.</h2>
+        <hr />
+      </div>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae laborum
         cupiditate at architecto ducimus, accusamus temporibus odit quisquam
